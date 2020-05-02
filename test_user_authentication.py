528e50d85257f48dfbf55e9f47580f8dd2b14368
Junwen-Zheng <54403387+Junwen-Zheng@users.noreply.github.com>
2020-05-03 05:16:26 +0930

    Add files via upload

--- test_user_authentication.py
+++ test_user_authentication.py
@@ -2,4 +2,9 @@ import requests
 import unittest
 
 class TestAuthenticationRestApi(unittest.TestCase):
-	pass
+	def __init__(self, *args, **kwargs):
+		super(TestAuthenticationRestApi, self).__init__(*args, **kwargs)
+		self.__api_base_url = "http://localhost:5000"
+		self.__secret_url = "/secret"
+		self.__user_url = "/user"
+
